1e744900dff44981b4a2f2b164770e60fb6e2270
Naia Shaffa <naiashaffa.2022@student.uny.ac.id>
2023-02-28 23:03:08 +0700

    new and edit

---
+++ src/Pemrograman_2/NaiaShoppingList.java
@@ -0,0 +1,10 @@
+package Pemrograman_2;
+
+import java.util.ArrayList;
+
+public class NaiaShoppingList {
+    public static void main(String []args){
+        ArrayList<String>list = new ArrayList<String>();
+        
+    }
+}
