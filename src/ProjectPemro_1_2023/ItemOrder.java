2a88f00de923aa0d8b11431d8af07412589ac921
Naia Shaffa <naiashaffa.2022@student.uny.ac.id>
2023-03-01 23:28:59 +0700

    new

--- src/ProjectPemro_1_2023/ItemOrder.java
+++ src/ProjectPemro_1_2023/ItemOrder.java
@@ -1,3 +1,39 @@
 class ItemOrder {
+    private Item item;
+    private int quantity;
+
+    public ItemOrder(Item item, int quantity){
+        this.item = item;
+        this.quantity = quantity;
+    }
+
+    public Item getItem(){
+        return item;
+    }
+    public int getQuantity(){
+        return quantity;
+    }
     
+    public double getPrice(){
+        if(item.getName(). equals("Tissu")){
+            int bulkSize = 2; 
+            double bulkPrice = item.getPrice() * 1.5;
+
+            int numBulkGrup = quantity / bulkSize;
+            int numNonBulkItem = quantity % bulkSize;
+
+            double total = numBulkGrup * bulkPrice + numNonBulkItem * item.getPrice();
+            return total;
+        }else if (item.getName(). equals("Apple")){
+            int bulkSize = 2;
+            double bulkPrice = item.getPrice() * 0.5;
+
+            int numBulkGrup = quantity / bulkSize;
+            int numNonBulkItem = quantity % bulkSize;
+
+            double total = numBulkGrup * bulkPrice + numNonBulkItem * item.getPrice();
+            return total;
+        } else 
+        return quantity * item.getPrice();
+    }
 }
