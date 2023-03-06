a414fe3e11ae3f6d6d574261c64277b9aa2e41c1
Naia Shaffa <naiashaffa.2022@student.uny.ac.id>
2023-03-06 14:05:13 +0700

    removeevens new

---
+++ src/Pemrograman_2/NaiaCh11RemoveEvens.java
@@ -0,0 +1,34 @@
+package Pemrograman_2;
+
+import java.util.*;
+
+public class NaiaCh11RemoveEvens {
+    public static void main(String [] args){
+        Set<Integer> s1 = new HashSet<Integer>();
+        Scanner console = new Scanner(System.in);
+
+        //inputan data menggunakan scanner
+        System.out.print("Jumlah data yang akan di input: ");
+        int jumlahData = console.nextInt();
+
+        for(int i = 0; i < jumlahData; i++){
+            System.out.printf("Masuka data ke-%d: ", i+1);
+            s1.add(console.nextInt());
+        }
+
+        System.out.println("Tampilkan data = " + s1);
+        System.out.print("Data genap = " + removeEvens(s1));
+    }
+    public static Set<Integer> removeEvens (Set<Integer> s1){
+        Set<Integer> s2 = new TreeSet<Integer>();
+        Iterator<Integer> ganjil = s1.iterator();
+        while (ganjil.hasNext()){
+            int num = ganjil.next();
+            if(num % 2 == 0){
+                s2.add(num);
+                ganjil.remove();
+            }
+        }
+        return s2;
+    }
+}
