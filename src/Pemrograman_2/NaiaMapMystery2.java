a0eda536d09df22df3ecebe85ecb8532bdb9b3c2
Naia Shaffa <naiashaffa.2022@student.uny.ac.id>
2023-03-06 08:21:57 +0700

    edit

---
+++ src/Pemrograman_2/NaiaMapMystery2.java
@@ -0,0 +1,34 @@
+package Pemrograman_2;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class NaiaMapMystery2 {
+    public static void main(String []args){
+        Set<String> si = new HashSet<String>();
+
+        si.add("sheep=wool");
+        si.add("house=brick");
+        si.add("cast=plaster");
+        si.add("wool=wool");
+
+        System.out.print("List awal : ");
+        System.out.println(si);
+
+        System.out.print("List hasil : ");
+
+    }
+    public static void mapMystery2(Map<String, String> m) {
+        Set<String> si = new TreeSet<String>();
+        for (String key : m.keySet()) {
+            if (!m.get(key).equals(key)) {
+                si.add(m.get(key));
+            } else {
+                si.remove(m.get(key));
+            }
+        }
+        System.out.println(si);
+    }
+}
