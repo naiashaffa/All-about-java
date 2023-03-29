89e1d38d525135031b510c63a7278c55b9b6c5c4
Naia Shaffa <naiashaffa.2022@student.uny.ac.id>
2023-03-29 23:02:59 +0700

    edit prak05

--- src/Struktur_data/prak05_22537144008.java
+++ src/Struktur_data/prak05_22537144008.java
@@ -3,54 +3,89 @@ package Struktur_data;
 //NIM   : 22537122008
 
 import java.util.*;
+//class untuk menyimpan data pasien
 class NodePas {
     public String namaPas;
     public String keluhanPas;
     public String alamatPas;
+    public NodePas next;
 
+// -------------------------------------------------------------
+    // constructor (memasukan data pasien)
     public NodePas (String nama, String keluhan, String alamat){
         namaPas = nama;
         keluhanPas = keluhan;
         alamatPas = alamat;
     }
-
+// -------------------------------------------------------------
+    // method untuk menampilkan data pasien
     public void lihatData(){
         System.out.println("Nama : " + namaPas + " || Keluhan : " + keluhanPas + " || Alamat : " + alamatPas);
         System.out.println("--------------------------------------------------------------------");
     }
-}
-class fisrtEndList {
-    private NodePas first;
-    private NodePas last;
+// -------------------------------------------------------------
+    // method untuk mengambil data  nama pasien
+    public String getNama() {
+        return namaPas;
+    }
 
-    public fisrtEndList() {
+    // method untuk mengambil data keluhan pasien
+    public String getKeluhan() {
+        return keluhanPas;
+    }
+
+    // method untuk mengambil data alamat pasien
+    public String getAlamat() {
+        return alamatPas;
+    }   
+} //end class pasien
+// -------------------------------------------------------------
+// class untuk menyimoan data pasien menggunnakan 'queue'
+class LinkQueue {
+    private NodePas first;          // pointer menunjuk data pertama
+    private NodePas last;           // pointer menunjuk data terakhir
+// -------------------------------------------------------------
+    // constructor
+    public LinkQueue() {
         first = null;
         last = null;
     }
-
+// -------------------------------------------------------------
+    // method untuk mengetahui kondisi antrian kosong atau ada
     public boolean isEmpty(){
-        return first == null;
+        return (first == null);
     }
-
-    public void insertLast (String nama, String keluhan, String alamat) {
-        NodePas newNodePas = new NodePas(nama, keluhan, alamat);
+// -------------------------------------------------------------
+    // method untuk menambah data pada queue (urutan paling belakang)
+    public void enQueue (NodePas PasienBaru) {
         if(isEmpty()) {
-            first = newNodePas;
+            first = PasienBaru;
         } else {
-            last.next = newNodePas;
-            last = newNodePas;
+            last.next = PasienBaru;
         }
+        last = PasienBaru;          // data yang di input terakhir akan otomatis ditunjuk oleh pointer last
+        System.out.println("");
+        System.out.println("Data pasien telah disimpan dan masuk ke dalam antrian.");
     }
-
-    public String hapusFirst() {
-        String temp = first.namaPas;
+// -------------------------------------------------------------
+    // method input untuk memasukan data ke dalam array
+    public void add (String namaPas, String keluhanPas, String alamatPas) {
+        NodePas dataBaru = new NodePas(namaPas, keluhanPas, alamatPas);
+        enQueue(dataBaru);          // memanggil method enQueue menggunakan parameter data baru untuk di masukan dalam antrian
+    }
+// -------------------------------------------------------------
+    // method untuk mengeluarkann data pada awal antrian
+    public NodePas deQueue() {
+        NodePas temp = first;
         if(first.next == null) {
             last = null;
-            first =first.next;
         }
+        first = first.next;
+
         return temp;
     }
-
+// -------------------------------------------------------------
+    // method untuk melihat data awal pada antrian pasien
     public void lihat() {
         NodePas index = first;
         while(index != null) {
@@ -59,128 +94,132 @@ class fisrtEndList {
         }
         System.out.println("");
     }
-}
-class LinkQueue {
-    private fisrtEndList list;
-
-    public LinkQueue() {
-        list = new fisrtEndList();
+// -------------------------------------------------------------
+    // method untuk memanggil pasien pada antrian selanjutnya
+    public NodePas terpanggil() {
+        return deQueue();       // memmangggil method deQueue untuk memanggil data pasien selanjutnya
     }
-
-    public boolean isEmpty() {
-        return list.isEmpty();
-    }
-
-    public boolean isFull(String a, String) {
-        if
-    }
-
-    public void enQueue(Strig namaPas, String keluhanPas, String alamatPas) {
-        list.insertLast(alamatPas, keluhanPas, alamatPas);
-    }
-
-    public long deQueue() {
-        return  list.hapusFirst();
-    }
-
-    public boolean clear() {
-        while (!list.isEmpty()) {
-            list.hapusFirst();
+// -------------------------------------------------------------
+    // method untuk mengosongkan antrian pasien
+    public void clear() {
+        while(!isEmpty()) {
+            deQueue();
         }
-        return list.isEmpty();
+    }
+// -------------------------------------------------------------
+    // method untuk mengetahui antrian pasien sudah penuh 
+    public boolean isFull(int cont, int max) {
+        if (cont == max) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+// -------------------------------------------------------------
+    // method untuk mengambil data nama pasien
+    public String namaPas() {
+        return first.getNama();
     }
 
-    public lihatQueue() {
-        System.out.println("Antrian (awal ke akhir) = ");
-        list.lihat();
+    // method untuk mengambil data keluhan pasien
+    public String keluhanPas() {
+        return first.getKeluhan();
     }
-}
+
+    // method untuk mengambil data alamat pasien
+    public String alamatPas() {
+        return first.getAlamat();
+    }
+} // end class Queue
+// -------------------------------------------------------------
 public class prak05_22537144008 {
     public static void main(String[] args) {
         LinkQueue queue = new LinkQueue();
         System.out.print("Masukan jumlah Antrian maksimal = ");
         Scanner input = new Scanner(System.in);
-        int max = input.nextInt();
-        int counter = 0;
+        int max = input.nextInt();      // nilai max jumlah antrian
+        int counter = 0;        
+
+        LinkQueue antrian = new LinkQueue();
+        String nama, keluhan, alamat;
 
         int menu;
 
         do {
-            System.out.println("######################################");
+            // berbagai menu yang tersedia dan dapat dipilih
+            System.out.println("================================");
 			System.out.println("Pilih Menu");
 			System.out.println("1. Pendaftaran Pasien");
 			System.out.println("2. Daftar Antrian Pasien");
-			System.out.println("3. Cari Pasien");
+			System.out.println("3. Reset Antrian");
 			System.out.println("4. Pemanggilan Pasien");
-			System.out.println("5. Keluar");
-            System.out.println("######################################");
+			System.out.println("5. Keluar");            // program dihentikan 
+            System.out.println("================================");
 			System.out.print("Menu yang dipilih : ");
-  	    	menu = input.nextInt();
+  	    	menu = input.nextInt();     // input menu yang dipilih
   	    	System.out.println("");
 //--------------------------------------------------------------
 			switch(menu) {
-				case 1 : {
-					if(!queue.isFull()) {
+				case 1 : {          // menu untuk menambahkan data pasien pada antrian
+					if(!queue.isFull(counter, max) == true) {       // memperhatikan jumlah data yang dapat diinput berdasarkan menthod isFull
                         System.out.println("----------------------");
                         System.out.println("  PENDAFTARAN PASIEN  ");
                         System.out.println("");
                         System.out.println("Masukan Data Pasien Baru");
                         System.out.print("Nama : ");
-                        nama = input.next();
+                        nama = input.next();        // input nama pasien
                         System.out.print("Keluhan : ");
-                        keluhan = input.next();
+                        keluhan = input.next();     // input keluhan pasien
                         System.out.print("Alamat : ");
-                        alamat = input.next();
-                        queue.daftar(nama, keluhan, alamat);
+                        alamat = input.next();      // input alamat pasien
+                        queue.add(nama, keluhan, alamat);         // memanggil method add yang ada dalam class queue
                         System.out.println("");
-                    } else {
+                        counter++;      // nilai counter akan bertambah dan akan berhenti saat nilai nya sama dengan max 
+                    } else {        // untuk memberitahu bahwa tidak dapat menambah data lagi karena antrian sudah penuh berdasarkan nilai max yang telah di input di awal
                         System.out.println("ANTRIAN PENUH !!!");
+                        System.out.println("");
                     }
 					break;
 				}
-				case 2 : {
+// -------------------------------------------------------------
+				case 2 : {      // menu untuk melihat daftar data pasien yang telah di input
 					System.out.println("----------------------");
-                    if(!queue.isEmpty()){
+                    if(!queue.isEmpty()){       // saat antrian tidak kosong maka akan ditampilkan seluruh data pasien yang telah tersimpan
                         System.out.println("    DAFTAR PASIEN ");
                         System.out.println("----------------------");
-                        queue.lihat();
+                        queue.lihat();          // memanggil method lihat yang ada pada class queue
                         System.out.println("");
                         System.out.println("");
-                    }else {
+                    } else {        // untuk memberitahu kondisi data antrian yang tersimpan kosong
                         System.out.println("xx ANTRIAN KOSONG xx ");
                         System.out.println("");
-                        System.out.println("");
                     }
 					break;
 				}
-				case 3 : {
-                    System.out.print("Nama Pasien :");
-                    String cari = input.next();
-                    pasien hasilCari = queue.cari(cari);
-                    if(hasilCari != null){
-                        hasilCari.lihatData();
-                        System.out.println("");
-                    }else {
-                        System.out.println("");
-                        System.out.println("Pasien tidak ditemukan pada daftar.");
-                        System.out.println("");
-                    }
+// -------------------------------------------------------------
+				case 3 : {      // menu untuk mereset data atau menghapus semua data pasien tanpa terkecuali
+                    queue.clear();      // memanggil method clear yand ada pada class queue
+                    System.out.println("Daftar Antrian Telah di Reset");
+                    System.out.println("");
 					break;
 				}
-				case 4 : {
-                    if(!queue.isEmpty()){
-                        pasien terpanggil = queue.dipanggil();
+// -------------------------------------------------------------
+				case 4 : {      // menu untuk memanggil pasien 
+                    if(!queue.isEmpty()){       // dapat memanggil pasien saat antrian tidak kosong
+                        NodePas terpanggil = queue.terpanggil();        // memanggil method terpanggil yang ada pada class queue
                         System.out.println("Panggilan kepada pasien atas nama " + terpanggil.getNama() + " diharap menuju ruang 1.");
                         System.out.println("");
-                        i--;
-                    }else {
+                        counter--;      // nilai counter akan berkurang (karena pasien setelah terpanggil akann di hapus)
+                    }else {     // untuk memberitahu kondisi data antrian yang tersimpan kosong
                         System.out.println("xx DAFTAR ANTRIAN KOSONG xx ");
-                        System.out.println("");
                         System.out.println("");
                     }
                 }
+// -------------------------------------------------------------
             }
-        } while(menu >= 1 && menu < 5);
-    }
-}
+        } while(menu >= 1 && menu < 5);         // mneu dapat dijalankan pada menu 1-4, 5 program berhenti
+        System.out.println("Progam sudah dihentikan. ");
+        System.out.println("");
+    } // end public void main
+} // end class
 
